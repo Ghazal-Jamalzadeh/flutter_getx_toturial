16038760dbd78974fa3330dcbada1ea992441f47
ghazal-jamalzadeh <jmzd.developr94@gmail.com>
2023-08-18 16:44:16 +0430

    snack bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -20,17 +20,76 @@ class MainScreen extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     /* Toast */
-                    MainRowItem(title: 'Show Toast', color: Colors.red, onTap:(){
-
-                    } )
+                    MainRowItem(
+                        title: 'Show Toast',
+                        onTap: () {
+                          showGetXToast();
+                        }),
                   ],
                 ),
               ),
             ),
           ),
-
         ),
       ),
     );
+  }
+
+  void showGetXToast() {
+    Get.snackbar('Snack bar Title', 'Salam Sosis',
+        snackPosition: SnackPosition.BOTTOM,
+        // titleText: Text('New Title' , style: TextStyle(color: Colors.red),) ,
+        // messageText: Text('Salam Kalbas' , style: TextStyle(color: Colors.blue),)
+        colorText: Colors.black87,
+        backgroundColor: Colors.red.withOpacity(0.3),
+        borderRadius: 8,
+        margin: const EdgeInsets.only(top: 56, left: 16, right: 16, bottom: 56),
+        // maxWidth: 100 ,
+        animationDuration: const Duration(seconds: 3),
+        duration: const Duration(seconds: 10),
+        backgroundGradient:
+            const LinearGradient(colors: [Colors.red, Colors.blue]),
+        borderWidth: 2,
+        borderColor: Colors.black87,
+        // boxShadows: [
+        //   const BoxShadow(
+        //       color: Colors.deepOrange,
+        //       offset: Offset(8, 8),
+        //       spreadRadius: 8,
+        //       blurRadius: 100)
+        // ],
+        isDismissible: true,
+        dismissDirection: DismissDirection.horizontal,
+        forwardAnimationCurve: Curves.bounceInOut,
+        // reverseAnimationCurve: Curves.bounceInOut,
+        icon: const Icon(Icons.ac_unit_outlined),
+        shouldIconPulse: true,
+        // leftBarIndicatorColor: Colors.green ,
+        mainButton: TextButton(
+          onPressed: () {
+            print('Button clicked...');
+          },
+          child: const Text('Click me!'),
+        ),
+        onTap: (snack) {
+          print('Snack bar clicked...');
+        },
+        overlayBlur: 5,
+        // overlayColor: Colors.deepOrange ,
+        // padding: EdgeInsets.all(32) ,
+        showProgressIndicator: false,
+        progressIndicatorBackgroundColor: Colors.black87,
+        progressIndicatorValueColor: const AlwaysStoppedAnimation(Colors.white),
+        // userInputForm: Form(child:
+        // Row(
+        //   children: [
+        //     Expanded(child:
+        //     TextField())
+        //   ],
+        // )
+        // ),
+        snackbarStatus: (val) {
+          print(val);
+        });
   }
 }
--- lib/widgets/main_row_item.dart
+++ lib/widgets/main_row_item.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 
 class MainRowItem extends StatelessWidget {
   String title ;
-  Color color ;
   Function onTap ;
-  MainRowItem({Key? key , required this.title , required this.color, required this.onTap}) : super(key: key);
+  MainRowItem({Key? key , required this.title , required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton.extended(
-        backgroundColor: Colors.limeAccent,
-        onPressed: onTap(),
+        backgroundColor: Colors.red,
+        onPressed: (){
+          onTap() ;
+        },
         label: Text(title));
   }
 }
